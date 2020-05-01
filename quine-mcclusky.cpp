6e34dfae0d6bfc88b32ac8aafcc22c380d687d65
ERUD1T3 <josiasmoukpe@gmail.com>
2020-04-30 22:40:30 -0400

    completed quine mclusky

--- quine-mcclusky.cpp
+++ quine-mcclusky.cpp
@@ -6,10 +6,9 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     /* code */
-
 
     // string inputfile = argv[1];
     // string outputfile = argv[2];
@@ -17,9 +16,10 @@ int main(int argc, char** argv)
     string inputs = "A,B,C,D", // inputs to expression, separated by comma
         // minterms = "0,1,3,7,8,9,11,15",
         // minterms = "0,2,5,6,7,8,10,12,13,14,15",// minterms to expression, separated by comma
-        minterms = "0,2,3,4,5,6,7,8,9,10,11,12,13",
-           dontcare = "";
-
+        // minterms = "0,2,3,4,5,6,7,8,9,10,11,12,13",
+        minterms = "2,3,7,9,11,13",
+        dontcare = "1,10,15";
+    // dontcare = "";
 
     if (DEBUG)
     {
